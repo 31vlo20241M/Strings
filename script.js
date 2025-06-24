ae7895dee096c5601f969b8d3f73f09bbbff4703
Viviane <viviane.luggeri.oliveira@escola.pr.gov.br>
2025-06-24 11:17:48 +0000

    aula 5

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavrasChave = processaTexto();
-    
-    campoResultado.textContent = palavrasChave.join (",");
+    const palavrasChave = processaTexto(texto);
 
-    // código omitido
-
-function processaTexto(texto){
-let palavras = texto.split(/\P{L}+/u);
-    return palavras;
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
+    return palavras;
 }
